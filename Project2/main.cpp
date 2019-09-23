de4b8621cec063a61dce9f9cb88b742fc5592499
Chris <christinaknuds@gmail.com>
2019-09-23 17:10:27 +0200

    Added Project 2

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "armadillo"
+
+using namespace std;
+using namespace arma;
+
+int main(){
+
+}
+
