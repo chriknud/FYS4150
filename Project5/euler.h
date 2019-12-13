d880797f08d8daa5bfb5831a6914267c9d43a711
Christina Knudsen <42637671+chriknud@users.noreply.github.com>
2019-12-13 11:07:38 +0100

    Add files via upload

---
+++ Project5/euler.h
@@ -0,0 +1,15 @@
+#ifndef EULER_H
+#define EULER_H
+
+
+class Euler
+{
+public:
+    double m_dt;
+    Euler(double dt);
+    void integrateOneStep(class SolarSystem &system);
+    void velocityVerletOneStep(class SolarSystem &system);
+    void velocityVerletOneStepSunFixed(class SolarSystem &system);
+};
+
+#endif // EULER_H
